a65ebc8d203d8b08e5dcdf41ad2be47b683ddb2e
Lucas Berardi <lucas.bera@gmail.com>
2022-05-13 21:37:50 -0300

    Rotas adicionadas

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import Cardapio from './pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import Router from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Cardapio />
+    <Router />
   </React.StrictMode>
 );
