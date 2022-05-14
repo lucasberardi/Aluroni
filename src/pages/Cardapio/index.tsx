91ea4217378e5031a06ee93226c4fe65175989f8
Lucas Berardi <lucas.bera@gmail.com>
2022-05-13 22:06:08 -0300

    Adicionando scss de tema para normalizar estilo de titulo e container

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,4 +1,5 @@
 import styles from './Cardapio.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
 
 import Buscador from './Buscador';
 import { useState } from 'react';
@@ -12,7 +13,7 @@ export default function Cardapio() {
   const [ordenador, setOrdenador] = useState('');
   return (
     <section className={styles.cardapio}>
-      <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+      <h3 className={stylesTema.titulo}>Cardápio</h3>
       <Buscador busca={busca} setBusca={setBusca} />
       <div className={styles.cardapio__filtros}>
         <Filtros filtro={filtro} setFiltro={setFiltro} />
